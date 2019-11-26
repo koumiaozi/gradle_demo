9839a51c34131afd15d4f09caa518d0a4aa40035
mjkou <mjkou@thoughtworks.com>
2019-11-26 22:52:08 +0800

    [Angelkou] use base plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,10 +4,22 @@
  * This is a general purpose Gradle build.
  * Learn how to create Gradle builds at https://guides.gradle.org/creating-new-gradle-builds/
  */
+plugins {
+    id("base")
+}
+
 tasks.create<Copy>("copy") {
     description = "Copies sources to the dest directory"
     group = "Custom"
 
     from("src")
     into("dest")
+}
+
+tasks.create<Zip>("zip") {
+    description = "Archives sources in a zip file"
+    group = "Archive"
+
+    from("src")
+    archiveName = "basic-demo-1.0.zip"
 }
